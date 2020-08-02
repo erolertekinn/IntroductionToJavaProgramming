b14ab1b97925b4ecf4c8c0f9434a8a027aa16c6a
Erol Ertekin <erolertekinn.66@gmail.com>
2020-08-02 18:00:57 +0200

    Solutions

--- chapter04/Exercise04_11.java
+++ chapter04/Exercise04_11.java
@@ -1,9 +1,34 @@
 package chapter04;
 
+import java.util.Scanner;
+
 public class Exercise04_11 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Enter a decimal value (0 to 15)");
+		Scanner keyboard = new Scanner(System.in);
+		
+		int decimalValue = keyboard.nextInt();
+		
+		if((decimalValue>-1) && (decimalValue<10)){
+			System.out.println("The hex value is:"+decimalValue);
+			
+		}
+		else if( (decimalValue>10)&&(decimalValue<16) ){
+			switch(decimalValue){
+			case(10):System.out.println("The hex value is: A");break;
+			case(11):System.out.println("The hex value is: B");break;
+			case(12):System.out.println("The hex value is: C");break;
+			case(13):System.out.println("The hex value is: D");break;
+			case(14):System.out.println("The hex value is: E");break;
+			case(15):System.out.println("The hex value is: F");break;
+			
+			}
+		}
+		else {
+			System.out.println("Enter a value between 1-15");
+		}
+
 
 	}
 
--- chapter04/Exercise04_12.java
+++ chapter04/Exercise04_12.java
@@ -1,5 +1,25 @@
 package chapter04;
 
-public class Exercise04_12 {
+import java.util.Scanner;
 
+public class Exercise04_12 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter a hex digit: ");
+		char hexDigit = input.next().charAt(0);
+		
+		switch(hexDigit){
+		case('A'):System.out.println("The binary value is 1010");break;
+		case('B'):System.out.println("The binary value is 1011");break;
+		case('C'):System.out.println("The binary value is 1100");break;
+		case('D'):System.out.println("The binary value is 1101");break;
+		case('E'):System.out.println("The binary value is 1110");break;
+		case('F'):System.out.println("The binary value is 1111");break;
+		
+		}
+		if((int)hexDigit>70 || (int)hexDigit<60){
+			System.out.println("Invalid input");
+		}
+	}
 }
